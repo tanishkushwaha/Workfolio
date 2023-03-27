e6f6a4eb1799784577dc851202def41c06c2b7e3
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2023-03-27 13:33:15 +0530

    Added user_details DB collection

--- server.js
+++ server.js
@@ -14,7 +14,30 @@ const userSchema = mongoose.Schema({
     password: String
 })
 
+const userDetails = mongoose.Schema({
+    job_title: String,
+    first_name: String,
+    last_name: String,
+    email: String,
+    phone: Number,
+    country: String,
+    city: String,
+    address: String,
+    postal_code: String,
+    experience: String,
+    school: String,
+    degree: String,
+    start_date: Date,
+    end_date: Date,
+    school_city: String,
+    education_description: String,
+    social_links: String,
+    skills: String,
+    hobbies: String
+})
+
 const userModel = mongoose.model('users', userSchema)
+const userResumeModel = mongoose.model('user_details', userDetails)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -42,6 +65,27 @@ app.post('/signup', (req, res) => {
 
 app.post('/create', (req, res) => {
     console.log(req.body)
+    userResumeModel.create({
+        job_title: req.body.job_title,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        phone: req.body.phone,
+        country: req.body.country,
+        city: req.body.city,
+        address: req.body.address,
+        postal_code: req.body.postal_code,
+        experience: req.body.experience,
+        school: req.body.school,
+        degree: req.body.degree,
+        start_date: req.body.start_date,
+        end_date: req.body.end_date,
+        school_city: req.body.school_city,
+        education_description: req.body.education_description,
+        social_links: req.body.social_links,
+        skils: req.body.skills,
+        hobbies: req.body.hobbies
+    })
     res.redirect('/')
 })
 
