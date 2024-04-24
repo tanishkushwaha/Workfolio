871b763a32f6c98c1a936d6f5cf8c71cc4df3242
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-04-24 21:43:26 +0530

    Created Home page

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import Footer from "../components/Footer"
+import Hero from "../components/Hero"
+import Section_1 from "../components/Section_1"
+import Section_2 from "../components/Section_2"
+import Section_3 from "../components/Section_3"
+import Section_4 from "../components/Section_4"
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Section_1 />
+      <Section_2 />
+      <Section_3 />
+      <Section_4 />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
