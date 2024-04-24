00ebb52d1d433b6e629077fee704a08e76bc2ebd
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-04-24 21:40:59 +0530

    Created Footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,73 @@
+
+
+const Footer = () => {
+  return (
+    <div className="container-fluid footer">
+      <div className="row">
+        <div className="col-md-1"></div>
+        <div className="col-md-2">
+          <h4 className="font-semibold">Job Seekers</h4>
+          <br />
+          <a href="/create">Build a Resume</a>
+          <br /><br />
+          <a href="/dummy_page">Samples</a>
+          <br /><br />
+          <a href="/dummy_page">Cover Letter Samples</a>
+          <br /><br />
+          <a href="/dummy_page">Apps</a>
+        </div>
+        <div className="col-md-2">
+          <h4 className="font-semibold">Resources</h4>
+          <br />
+          <a href="/dummy_page">Career Advice</a>
+          <br /><br />
+          <a href="/dummy_page">Resumes</a>
+          <br /><br />
+          <a href="/dummy_page">Career Development</a>
+          <br /><br />
+          <a href="/dummy_page">Getting a Job</a>
+          <br /><br />
+          <a href="/dummy_page">Cover Letters</a>
+          <br /><br />
+          <a href="/dummy_page">Interviewing</a>
+        </div>
+        <div className="col-md-2">
+          <h4 className="font-semibold">Need Help?</h4>
+          <br />
+          <a href="/dummy_page">Help Center</a>
+          <br /><br />
+          <a href="">About Us</a>
+          <br /><br />
+          <a href="/dummy_page">Sitemap</a>
+        </div>
+        <div className="col-md-2">
+          <h4 className="font-semibold">Language</h4>
+          <br />
+          <a href="">English</a>
+          <br /><br />
+          <a href="">English(UK)</a>
+          <br /><br />
+          <a href="">Italiano</a>
+          <br /><br />
+          <a href="">Español</a>
+          <br /><br />
+          <a href="">Français</a>
+        </div>
+        <div className="col-md-2">
+          <h4 className="font-semibold">Stay Connected</h4>
+          <br />
+          <a href="https://www.facebook.com">Facebook</a>
+          <br /><br />
+          <a href="https://www.twitter.com">Twitter</a>
+          <br /><br />
+          <a href="https://www.linkedin.com">LinkedIn</a>
+          <br /><br />
+          <a href="https://www.pinterest.com">Pinterest</a>
+        </div>
+        <div className="col-md-1"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
