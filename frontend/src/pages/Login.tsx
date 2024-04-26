e686e2e3ba20d46b7184e6c124673cd3d7920d12
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-04-26 19:59:07 +0530

    Added logic to the auth forms

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,20 +1,68 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import Spinner from "../components/Spinner"
+import axiosInstance from "../utils/axiosInstance"
+
 const Login = () => {
 
-  const handleSubmit = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+
+    setIsLoading(true)
+    axiosInstance.post('/login', data, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res) => {
+        setIsLoading(false)
+        console.log(res);
+
+        if (res.status === 200) {
+          navigate('/create')
+        }
+      })
+
+      .catch((err) => {
+        setIsLoading(false)
+        console.log(err);
+      })
+  }
+
+  const handleInput = (e: any) => {
+    const { name, value } = e.target
+
+    setData((prevData) => (
+      {
+        ...prevData,
+        [name]: value,
+      }
+    ))
   }
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="auth-form d-flex flex-column justify-content-around align-items-center mt-5">
-        <h1 className='mb-4'>Log in</h1>
-        <form className="d-flex flex-column justify-content-between row-gap-2 w-75">
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <div className="d-flex justify-content-center">
-            <button className="btn mt-3" onClick={handleSubmit}>Log in</button>
-          </div>
-        </form>
-      </div>
+      {isLoading ? <Spinner /> : (
+        <div className="auth-form d-flex flex-column justify-content-around align-items-center mt-5">
+          <h1 className='mb-4'>Log in</h1>
+          <form className="d-flex flex-column justify-content-between row-gap-2 w-75" onSubmit={handleSubmit}>
+            <input className="form-control" type="email" name="email" placeholder="Email" value={data.email} onChange={handleInput} required />
+            <input className="form-control" type="password" name="password" placeholder="Password" value={data.password} onChange={handleInput} required />
+            <div className="d-flex justify-content-center">
+              <button type="submit" className="btn mt-3">Log in</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,28 +1,93 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import Spinner from "../components/Spinner"
+
 const Register = () => {
 
-  const handleSubmit = () => {
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+
+    if (data.password !== data.confirmPassword) {
+      alert("Passwords do not match!")
+      return
+    }
+
+    setIsLoading(true)
+    axios.post('/register', data, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((res: any) => {
+        setIsLoading(false)
+        console.log(res);
+
+        if (res.status === 200) {
+          navigate('/login')
+        }
+      })
+      .catch((err) => {
+        setIsLoading(false)
+        console.log(err);
+      })
+  }
+
+  const handleInput = (e: any) => {
+    const { name, value } = e.target
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
   }
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="auth-form d-flex flex-column justify-content-around align-items-center mt-5">
-        <h1 className='mb-4'>Sign up</h1>
-        <form className="d-flex flex-column justify-content-between row-gap-2 w-75">
-          <input type="text" name="firstName" placeholder="First Name" />
-          <input type="text" name="LastName" placeholder="Last Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="password" name="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <div className="d-flex mt-4">
-            <input className="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault" />
-            <label className="form-check-label" htmlFor="flexCheckDefault">By signing up you agree to Term
-              of service and Privacy Policy</label>
-          </div>
-          <div className="d-flex justify-content-center">
-            <button className="btn mt-3" onClick={handleSubmit}>Sign up</button>
-          </div>
-        </form>
-      </div>
+      {isLoading ? <Spinner /> : (
+        <div className="auth-form d-flex flex-column justify-content-around align-items-center mt-5">
+
+          <h1 className='mb-4'>Sign up</h1>
+
+          <form className="d-flex flex-column justify-content-between row-gap-2 w-75" onSubmit={handleSubmit}>
+
+            <input className="form-control" type="text" name="firstName" placeholder="First Name" value={data.firstName} onChange={handleInput} required />
+
+            <input className="form-control" type="text" name="lastName" placeholder="Last Name" value={data.lastName} onChange={handleInput} required />
+
+            <input className="form-control" type="email" name="email" placeholder="Email" value={data.email} onChange={handleInput} required />
+
+            <input className="form-control" type="password" name="password" placeholder="Password" value={data.password} onChange={handleInput} required />
+
+            <input className="form-control" type="password" name="confirmPassword" placeholder="Confirm Password" value={data.confirmPassword} onChange={handleInput} required />
+
+            <div className="d-flex mt-4">
+
+              <input className="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault" required />
+
+              <label className="form-check-label" htmlFor="flexCheckDefault">By signing up you agree to Term
+                of service and Privacy Policy</label>
+
+            </div>
+
+            <div className="d-flex justify-content-center">
+              <button className="btn mt-3" type="submit">Sign up</button>
+            </div>
+
+          </form>
+        </div>
+      )}
     </div>
   )
 }
