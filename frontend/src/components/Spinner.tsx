1d73965c5dd2e64cfd4881c3b63abe547ae00911
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-04-26 19:57:51 +0530

    Initial commit

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+
+
+const Spinner = () => {
+  return (
+    <div className="spinner-grow text-primary" style={{ height: '3rem', width: '3rem' }} role="status">
+      <span className="visually-hidden">Loading...</span>
+    </div>
+  )
+}
+
+export default Spinner
