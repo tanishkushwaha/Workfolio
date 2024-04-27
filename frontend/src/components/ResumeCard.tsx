f4c213b58b3366d934a57e0cb56532d5edb2df76
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-04-27 20:06:42 +0530

    Initial commit

---
+++ frontend/src/components/ResumeCard.tsx
@@ -0,0 +1,11 @@
+import { Card } from "react-bootstrap"
+
+const ResumeCard = () => {
+  return (
+    // <div className="bg-primary" style={{ width: '300px', height: '200px' }}></div>
+
+    <Card bg='secondary' style={{ width: '20rem', height: '12rem' }} />
+  )
+}
+
+export default ResumeCard
