cf2b161b47a879dca88ab6e4139d3f5b1dd1f3cb
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2024-04-30 18:45:40 +0530

    Initial commit

---
+++ backend/src/routes/userRoute.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from "express";
+import auth from "../middlewares/auth";
+import ExtendedRequest from "../utils/ExtendedRequest";
+
+const router = Router();
+
+router.get("/", auth, (req: ExtendedRequest, res: Response) => {
+  return res.status(200).send(req.user);
+});
+
+export default router;
